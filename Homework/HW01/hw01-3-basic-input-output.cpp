2f0c30f3aee63375984211c53d55479a1eefbe32
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-14 18:39:36 +0700

    update hw1-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -11,20 +11,20 @@
         First Name: ABC
         Last Name: DEF
     Output:
-        ABD DEF TC, RMUTL, Chiang Mai, Thailand
+        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
 */
 #include <stdio.h>
 
 int main() {
     
     char first_name[50], last_name[50];
-    // รับชื่อจากผู้ใช้
+
     printf("First Name: ");
     scanf("%s", first_name);
     printf("Last Name: ");
     scanf("%s", last_name);
-    // แสดงผล
-    printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n", first_name, last_name);
+
+    printf("%s %ss TC, RMUTL, Chiang Mai, Thailand\n", first_name, last_name);
 
     return 0;
 }//end function
