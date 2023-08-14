e22f4f65ea5d50e34c0c5223301de30fc597adde
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-14 11:28:29 +0700

    update hw1-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -13,3 +13,31 @@
     Output:
         ABD DEFs TC, RMUTL, Chiang Mai, Thailand
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char first_name[50], last_name[50];
+
+    // รับชื่อจากผู้ใช้
+    printf("First Name: ");
+    scanf("%s", first_name);
+    printf("Last Name: ");
+    scanf("%s", last_name);
+
+    // ประกาศและกำหนดค่าให้ที่อยู่
+    char address[] = "TC, RMUTL, Chiang Mai, Thailand";
+
+    // แปลงนามสกุลให้เป็นรูปแบบที่ต้องการ
+    int last_name_length = strlen(last_name);
+    last_name[last_name_length - 1] = 's';
+
+    // แสดงผลลัพธ์
+    printf("Output:\n");
+    printf("%s %s %s\n", first_name, last_name, address);
+
+    return 0;
+}//end function
+
+
